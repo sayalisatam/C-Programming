107a9c12832b45c14c346af2dff53ffe5564bae8
sayalisatam <sayalisatam14@gmail.com>
2020-10-21 10:58:06 +0530

    Take user input from keyboard

---
+++ HelloWorldV1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	char name[20];
+	printf("Enter your First Name: /n");
+	scanf("%s",name);
+	printf("Your name is %s, Welcome to GitHub world\n",name);
+	return 0;
+}
